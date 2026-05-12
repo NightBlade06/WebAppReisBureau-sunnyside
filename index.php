c09a3e16acf2cd971a619d34c548f8e6472e5ae7
NightBlade06 <dylanwijnands1@gmail.com>
2026-05-12 11:37:55 +0200

    intial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <form action="" method="get">
+
+            <select name="" id="">
+                <option value="vliegreis">vliegreis</option>
+                <option value="treinreis">treinreis</option>
+            </select>
+
+            <input type="number">
+
+            <input type="submit">
+        </form>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
