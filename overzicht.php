8fd034489fe2411ca5edd3423d638d9d786da7ef
Hezjz <jansmit171104@gmail.com>
2026-05-29 11:25:02 +0200

    added a arrow up and down png and set a basic html structure for the overzicht page

--- overzicht.php
+++ overzicht.php
@@ -7,53 +7,65 @@
     <link rel="stylesheet" href="assets/css/overzicht.css">
 </head>
 <body>
-
 <header>
-    <img src="assets/img/header-img.jpg" alt="Header-img">
+    <img src="assets/img/header-img.jpg" alt="header-img">
 </header>
-
 <main>
-    <form class="searchForm">
-        <div class="searchInputBox">
-            <p>Destination</p>
-            <Select class="searchSelect" name="destination">
-                <option class="searchOption" value="0">Greek</option>
-                <option class="searchOption" value="1">Eqypt</option>
-                <option class="searchOption" value="2">Italy</option>
-                <option class="searchOption" value="4">Spain</option>
-            </Select>
+    <div class="searchBox">
+        <form class="searchForm">
+            <div class="searchInputBox">
+                <p>Destination</p>
+                <Select class="searchSelect" name="destination">
+                    <option class="searchOption" value="0">Greek</option>
+                    <option class="searchOption" value="1">Eqypt</option>
+                    <option class="searchOption" value="2">Italy</option>
+                    <option class="searchOption" value="4">Spain</option>
+                </Select>
+            </div>
+            <div class="searchInputBox">
+                <p>Depature date</p>
+                <Select class="searchSelect" name="destination">
+                    <option class="searchOption" value="5">14 may</option>
+                    <option class="searchOption" value="6">12 jan</option>
+                    <option class="searchOption" value="7">48 dec</option>
+                    <option class="searchOption" value="8">13 nov</option>
+                </Select>
+            </div>
+            <div class="searchInputBox">
+                <p>Duration</p>
+                <Select class="searchSelect" name="destination">
+                    <option class="searchOption" value="9">1 week</option>
+                    <option class="searchOption" value="10">2 weeks</option>
+                    <option class="searchOption" value="11">3 weeks</option>
+                    <option class="searchOption" value="12">4 weeks</option>
+                </Select>
+            </div>
+            <div class="searchInputBox">
+                <p>Travellers</p>
+                <Select class="searchSelect" name="destination">
+                    <option class="searchOption" value="13">1</option>
+                    <option class="searchOption" value="14">2</option>
+                    <option class="searchOption" value="15">3</option>
+                    <option class="searchOption" value="16">4</option>
+                </Select>
+            </div>
+            <div class="searchButtonBox">
+                <button class="searchButton" type="submit"> View our choices</button>
+            </div>
+        </form>
+    </div>
+    <div>
+        <div class="customizationBox">
+            <div>
+            bestemming
+            </div>
         </div>
-        <div class="searchInputBox">
-            <p>Depature date</p>
-            <Select class="searchSelect" name="destination">
-                <option class="searchOption" value="5">14 may</option>
-                <option class="searchOption" value="6">12 jan</option>
-                <option class="searchOption" value="7">48 dec</option>
-                <option class="searchOption" value="8">13 nov</option>
-            </Select>
+        <div>
+            <div>
+
+            </div>
         </div>
-        <div class="searchInputBox">
-            <p>Duration</p>
-            <Select class="searchSelect" name="destination">
-                <option class="searchOption" value="9">1 week</option>
-                <option class="searchOption" value="10">2 weeks</option>
-                <option class="searchOption" value="11">3 weeks</option>
-                <option class="searchOption" value="12">4 weeks</option>
-            </Select>
-        </div>
-        <div class="searchInputBox">
-            <p>Travellers</p>
-            <Select class="searchSelect" name="destination">
-                <option class="searchOption" value="13">1</option>
-                <option class="searchOption" value="14">2</option>
-                <option class="searchOption" value="15">3</option>
-                <option class="searchOption" value="16">4</option>
-            </Select>
-        </div>
-        <div class="searchButtonBox">
-            <button class="searchButton" type="submit"> View our choices</button>
-        </div>
-    </form>
+    </div>
 </main>
 
 <footer>
